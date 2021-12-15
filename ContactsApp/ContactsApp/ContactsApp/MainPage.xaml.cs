92519c1d60b994015530034d31454d778cc939f5
Kamil <kurbonovkamil@gmail.com>
2021-12-15 13:48:17 +0800

    Update MainPage.xaml.cs

--- ContactsApp/ContactsApp/ContactsApp/MainPage.xaml.cs
+++ ContactsApp/ContactsApp/ContactsApp/MainPage.xaml.cs
@@ -2,10 +2,12 @@
 using ContactsApp.ViewModels;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Essentials;
 using Xamarin.Forms;
 
 namespace ContactsApp
@@ -16,8 +18,15 @@ namespace ContactsApp
         {
             InitializeComponent();
 
-            var contactsRepo = new ContactsRepository();
-            BindingContext = new MainViewModel(contactsRepo);
+            var permissionService = new PermissionService();
+            var contactsService = new ContactsService();
+            var contactsRepo = new ContactsRepository(contactsService);
+            BindingContext = new MainViewModel(contactsRepo, permissionService);
+        }
+
+        protected async override void OnAppearing()
+        {
+            base.OnAppearing();
         }
     }
 }
